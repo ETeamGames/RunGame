46848ed0e3867df160629e6e1c836d1ff76e7dc0
ponJuice <matsu_tetsu_tty@yahoo.co.jp>
2015-10-29 00:56:10 +0900

    最初のコミット

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float power = 0;
+    public dynamicObjectScript[] objects = new dynamicObjectScript[3];
+
+    //攻撃可能物体を座標に飛ばす
+    public void attack(Vector3 vec)
+    {
+        foreach(dynamicObjectScript obj in objects)
+        {
+            if (obj != null)
+            {
+                //objectとタッチ位置との方向ベクトルを算出
+                Vector2 direction = vec - obj.transform.position;
+                Debug.Log("<PlayerScript:attack> direction" + direction);
+                //速度を0にする
+                obj.debugScript.debug = false;
+                //その方向に力を加える
+                obj.objectRigidbody.AddForce(direction.normalized * power);
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ Assets/Scripts/backgroundScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundScript : MonoBehaviour {
+    public float scrollSpeed = 0f;
+
+    void Awake()
+    {
+        //nextBackground.GetComponent<backgroundScript>().scrollSpeed = scrollSpeed;
+    }
+	// Use this for initialization
+	void Start () {
+        //nextBackground = GameObject.Find("background");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Time.deltaTime * scrollSpeed, 0, 0, Space.World);
+	}
+    
+}
