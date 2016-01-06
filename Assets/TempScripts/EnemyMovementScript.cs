59424d7de35067e6c8c2100f9079397d876c5f83
kanura <is0127kh@ed.ritsumei.ac.jp>
2016-01-06 20:04:37 +0900

    add scripts
    カメラ用スクリプトを追加
    プレイヤーが停止時もカメラ自動移動

--- Assets/TempScripts/EnemyMovementScript.cs
+++ Assets/TempScripts/EnemyMovementScript.cs
@@ -3,17 +3,11 @@ using UnityEngine.UI;
 using System;
 using System.Collections.Generic;
 
-public class MoveScript : MonoBehaviour {
-	[SerializeField]
-	private Camera cam;
+public class EnemyMovementScript : MonoBehaviour {
 	[SerializeField]
 	private Rigidbody2D target;
 	[SerializeField]
 	private Vector3 velocity;
-	[SerializeField]
-	private int camMoveDelay;
-	[SerializeField]
-	public GameObject ghostPlayer;
 	// direction of the player's movement, should be given 1 or -1
 	public float direction;
 	private Vector2 myScale;
@@ -34,7 +28,7 @@ public class MoveScript : MonoBehaviour {
 			target.velocity = value;
 		}
 	}
-
+	
 	void Awake()
 	{
 		
@@ -45,44 +39,17 @@ public class MoveScript : MonoBehaviour {
 		myScale = transform.localScale;
 	}
 	
-	void FixedUpdate()
-	{
+	void FixedUpdate() {
 		nextVector = new Vector3 (Velocity.x * direction, target.velocity.y, 0);
-		if (moveOn && nextVector.x != -1) {
+		if (moveOn) {
 			// update direction of the player
 			myScale.x *= direction;
 			transform.localScale = myScale;
 			target.velocity = nextVector;
-			//Debug.Log ("Player = "+target.velocity.x);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {   
-
-	}
-	
-	// stop the player's movement
-	public void StopPlayerMovement () {
-		moveOn = false;
-		Debug.Log ("call StopPlayerMovement ()");
-	}
-	
-	// resume the player's movement
-	public void ResumePlayerMovement () {
-		moveOn = true;
-		Debug.Log ("call ResumePlayerMovement ()");
-	}
-	
-	// use this when the player get to the Goal
-	void OnTriggerEnter2D (Collider2D col) {
-		if (col.gameObject.tag == "Goal") {
-			Debug.Log("Collision Goal");
-			direction = -1;
-		}
-		// use for test
-		if (col.gameObject.tag == "Test") {
-			Debug.Log("Collision Test Object.");
-		}
 	}
 }
